9d35f4243d53d097a905f532ad304f6c838db643
Anderson <andersonsilvapp@gmail.com>
2023-10-23 19:38:26 +0000

    project config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
+const { redirect } = require('next/dist/server/api-utils')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/bank-accounts',
+        permanent: false,
+      },
+    ]
+  }
+}
 
 module.exports = nextConfig
